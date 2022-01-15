ca0527e6c827932eaeb85d81071f937dd4863dfe
toan1804 <nguyenvantoan1804@gmail.com>
2022-01-15 21:27:29 +0700

    Fix bug

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -20,7 +20,7 @@ dishRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post(authennticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Dishes.create(req.body)
     .then((dish) => {
         console.log('Dish Created ', dish);
