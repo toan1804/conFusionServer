0889966887e106c23a2d260f08f2e71a6bb87109
toan1804 <nguyenvantoan1804@gmail.com>
2022-01-16 11:03:15 +0700

    File Upload

---
+++ routes/uploadRouter.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const authenticate = require('../authenticate');
+const multer = require('multer');
+
+//* Note: The multer middleware is used to parse the form data.
+// cb is a callback function that is called when the file has been uploaded.
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images');
+    },
+
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+const imageFileFilter = (req, file, cb) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+        return cb(new Error('You can upload only image files!'), false);
+    }
+    cb(null, true);
+};
+
+const upload = multer({ storage: storage, fileFilter: imageFileFilter });
+
+const uploadRouter = express.Router();
+
+uploadRouter.use(bodyParser.json());
+
+
+//* Note: the `post` method is used to upload the image.
+//* The `upload.single()` method is used to upload a single file, `imageFile` is the name of the form field.
+uploadRouter.route('/')
+.get(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    res.statusCode = 403;
+    res.end('GET operation not supported on /imageUpload');
+})
+.post(authenticate.verifyUser, authenticate.verifyAdmin, upload.single('imageFile'), (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(req.file);
+})
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /imageUpload');
+})
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    res.statusCode = 403;
+    res.end('DELETE operation not supported on /imageUpload');
+});
+
+module.exports = uploadRouter;
