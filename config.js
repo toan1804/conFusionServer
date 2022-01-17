1e95d6c0f12a0a33d35ba9f5b018a9f289ebe8a1
toan1804 <nguyenvantoan1804@gmail.com>
2022-01-17 08:35:53 +0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '654724142549125',
+        clientSecret: '3cc408dc4f040c4edbf6ff93886cde3d'
+    }
 }
